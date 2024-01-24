b7e309e42f1d4b52fa22768d957740d812aba309
timurlog <timur.logie@gmail.com>
2024-01-24 12:35:54 +0100

    modified index.html main.js _section-1.sass cards system done

---
+++ public/js/main.js
@@ -0,0 +1,83 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const memoryCards = document.querySelectorAll('.memory-card');
+    const cardsArray = Array.from(memoryCards);
+    const randomCardsArray = randomCards(cardsArray);
+
+    const memoryContent = document.querySelector('.memory-content .memory-cards');
+    memoryContent.innerHTML = '';
+    randomCardsArray.forEach(card => {
+        memoryContent.appendChild(card);
+    });
+});
+
+function randomCards(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+let cards = document.querySelectorAll('.memory-card');
+
+function checkAll() {
+    cards = document.querySelectorAll('.memory-card');
+    let i = 0;
+    cards.forEach(card => {
+        if (card.classList.contains('active')) {
+            i += 1
+        }
+        
+        if (i >= 2) {
+            cards.forEach(card => {
+                if (card.classList.contains('active')) {
+                    card.classList.remove('active');
+                    card.firstElementChild.classList.remove('back-flip');
+                    card.lastElementChild.classList.remove('front-flip');
+                }
+            });
+        }
+    });
+}
+
+function userSelection(x) {
+    if (x.classList.contains('done')) {
+        return
+    }
+    else
+    {
+        x.classList.add('active');
+        x.firstElementChild.classList.add('back-flip');
+        x.lastElementChild.classList.add('front-flip');
+    }
+}
+
+function checkSelection() {
+    cards = document.querySelectorAll('.memory-card');
+    let selectedArray = [];
+    cards.forEach(card => {
+        if (card.classList.contains('active')) {
+            selectedArray.push(card);
+        }
+    });
+
+    if (selectedArray.length == 2) {
+        if (selectedArray[0].classList.value == selectedArray[1].classList.value) {
+            selectedArray[0].classList.remove('active');
+            selectedArray[1].classList.remove('active');
+            selectedArray[0].classList.add('done');
+            selectedArray[1].classList.add('done');
+        }
+    }
+    else {
+        return
+    }
+}
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        checkAll();
+        userSelection(card);
+        checkSelection();
+    })
+});
